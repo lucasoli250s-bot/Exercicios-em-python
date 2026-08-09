02613d542bf42b84ebdf1dc4dfeb7adadff734c4
Comodoro_oli <lucasoli250s@gmail.com>
2026-08-09 12:45:06 -0300

    Delete conversor de moeda.py

--- conversor de moeda.py
+++
@@ -1,5 +0,0 @@
-real = float(input("Digite quantos reais você tem na carteira: R$ "))
-
-dolar = real / 5.12 #Para converter a Moeda locar para outro , é apenas pegar o valor da moeda e dividor pela cotação.
-
-print("Com R${:.2f} você pode comprar US${:.2f}".format(real, dolar))
